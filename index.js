97b8f6ff251efcc02954ad606cb2108b08a041ac
chhayaa <32996528+chhayaa@users.noreply.github.com>
2025-02-07 13:56:45 +0530

    changes

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 const newUserModel = require('./models/new-user.model');
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://imeuswe-staging:imeuswe-staging@imeuswe-lower-env.bnbk7.mongodb.net/kapp-dev?retryWrites=true&w=majority&appName=imeuswe-lower-env", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected Successfully');
+  } catch (err) {
+    console.error('MongoDB Connection Error:', err);
+    throw err;
+  }
+};
+
+// Add connection event listeners
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to DB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+module.exports = connectDB;
+
 
 module.exports = {
   newUserModel
