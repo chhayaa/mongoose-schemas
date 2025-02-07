790ba95e2be8136660a91e03f9313c4d835e7b02
chhayaa <32996528+chhayaa@users.noreply.github.com>
2025-02-07 14:37:13 +0530

    changes

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose');
 
 mongoURI="mongodb+srv://imeuswe-staging:imeuswe-staging@imeuswe-lower-env.bnbk7.mongodb.net/kapp-dev?retryWrites=true&w=majority&appName=imeuswe-lower-env"
 const connectDB = async (mongoURI) => {
-    if (!mongoose.connection.readyState) {
+   // if (!mongoose.connection.readyState) {
         await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log("Connected to MongoDB from shared schema package");
-    }
+  //  }
 };
 
 module.exports = {
