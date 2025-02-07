1d1005b7c6ac38c3160b65e18977aefab3c129e1
chhayaa <32996528+chhayaa@users.noreply.github.com>
2025-02-07 14:52:52 +0530

    remove mongo connection

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const newUserModel = require('./models/new-user.model');
 const mongoose = require('mongoose');
 
 
-mongoURI="mongodb+srv://imeuswe-staging:imeuswe-staging@imeuswe-lower-env.bnbk7.mongodb.net/kapp-dev?retryWrites=true&w=majority&appName=imeuswe-lower-env"
-const connectDB = async (mongoURI) => {
-   // if (!mongoose.connection.readyState) {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Connected to MongoDB from shared schema package");
-  //  }
-};
+// mongoURI="mongodb+srv://imeuswe-staging:imeuswe-staging@imeuswe-lower-env.bnbk7.mongodb.net/kapp-dev?retryWrites=true&w=majority&appName=imeuswe-lower-env"
+// const connectDB = async (mongoURI) => {
+//    // if (!mongoose.connection.readyState) {
+//         await mongoose.connect(mongoURI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log("Connected to MongoDB from shared schema package");
+//   //  }
+// };
 
 module.exports = {
   connectDB,
